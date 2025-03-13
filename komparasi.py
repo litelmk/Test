c0479de1b60d413472f1df731c035f521f364c65
litelmk <malikiabdul300@gmail.com>
2025-03-13 14:04:42 +0700

    testing

---
+++ komparasi.py
@@ -0,0 +1,27 @@
+#operasi komparasi
+
+# setiap hasil dari komparasi adalah boolean
+# >, <, >=, <=, ==, !=, is, is not
+a = 10
+b = 5
+
+# Lebih besar dari
+hasil = a > 3
+print(a,'>',3,'=',hasil )
+hasil = a > 11
+print(a,'>',11,'=',hasil )
+hasil = b > 2
+print(b,'>',2,'=',hasil )
+hasil = b > 7
+print(b,'>',7,'=',hasil )
+
+# kurang dari dari
+hasil = a < 3
+print(a,'<',3,'=',hasil )
+hasil = a < 11
+print(a,'<',11,'=',hasil )
+hasil = b < 2
+print(b,'<',2,'=',hasil )
+hasil = b < 7
+print(b,'<',7,'=',hasil )
+
